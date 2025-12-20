72d3b33797c6815c5de6955781b066d2761e2cba
Uzair-khane <uzairkhanar123@gmail.com>
2025-12-20 21:59:57 +0500

    learn about a path moduel

---
+++ Node js Module/Path Moduel/moduel.js
@@ -0,0 +1,16 @@
+const path = require("path")
+
+let filePath = path.join("folder","file.txt")
+console.log(filePath)
+// this will show the absolute path 
+console.log(path.resolve(filePath))
+// this will show the Base name of the path which is file.txt 
+console.log(path.basename(filePath))
+// this will show the dir name which is folder 
+console.log(path.dirname(filePath))
+// this will show the file extension which is .txt 
+console.log(path.extname(filePath))
+// this will breaks the path into Object 
+console.log(path.parse(filePath))
+// this check ther path is the path is absolute or not it return true or false value 
+console.log(path.isAbsolute(filePath))
