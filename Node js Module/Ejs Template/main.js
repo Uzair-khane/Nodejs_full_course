0ce291c578ebc664dc27592c2cde50401df2dba9
Uzair-khane <uzairkhanar123@gmail.com>
2026-01-23 14:13:09 +0500

    learn form in ejs

--- Node js Module/Ejs Template/main.js
+++ Node js Module/Ejs Template/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:false}))
 // simple only render ejs file  
 app.get("/",(req,res)=>{
     res.render("home")
@@ -19,7 +20,15 @@ app.get("/profile",(req,res)=>{
     })
 })
 
-
+// form 
+app.get('/form',(req,res)=>{
+    res.render("form")
+})
+app.post("/submit",(req,res)=>{
+ const name = req.body.myname;
+ const message = `hello ${name} you login succsesfully`
+ res.send(message)
+})
 app.listen("3000",()=>{
     console.log("server start")
 })
